5dd5e0bdfc7ed32c28c855553667ec4e8fd339f4
unknown <jakirhossain45951@gmail.com>
2024-10-14 14:09:58 +0600

    first commit

---
+++ app.ts
@@ -0,0 +1,18 @@
+import cors from 'cors'
+
+// // parsers
+// app.use(express.json())
+// app.use(cors())
+
+// // routers
+// app.use('/api', UserRoutes)
+// app.use('/api', TeamRoutes)
+
+// //! error handler
+// app.use(globalError)
+
+// app.get('/', (req, res) => {
+//   res.send('Server running')
+// })
+
+// export default app
